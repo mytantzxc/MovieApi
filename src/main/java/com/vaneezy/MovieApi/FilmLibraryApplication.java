1170c75aa1dd629cc49027d6320096c00138748f
Andreev <ioanandreev95@gmail.com>
2023-02-03 05:48:05 +0300

    Commiting demo api

---
+++ src/main/java/com/vaneezy/MovieApi/FilmLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.vaneezy.MovieApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmLibraryApplication.class, args);
+	}
+
+}
